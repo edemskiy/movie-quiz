6c478f583e6338a03f2ccfae574626807b0cb476
Edemsky Alexander <qapple@ya.ru>
2020-04-12 14:41:05 +0300

    Add change app folder name in server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/quiz-movie"));
+app.use(express.static(__dirname + "/dist/movie-quiz"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/quiz-movie/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/movie-quiz/index.html"));
 });
 
 app.listen(process.env.PORT || 5000);
